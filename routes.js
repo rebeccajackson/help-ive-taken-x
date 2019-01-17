6f9bd6b123546436ea3a108f9106d6f961598b4a
student <mukherjeei@outlook.co.nz>
2019-01-18 09:28:25 +1300

    add class to data.json

--- routes.js
+++ routes.js
@@ -3,7 +3,10 @@ const router = express.Router()
 const fs = require('fs')
 
 router.get('/', function (req, res) {
+    fs.readFile('./data.json','utf8', function(err,res))
     res.render('home')
 })
+
+
 
 module.exports = router
