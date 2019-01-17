a5100fe775d05f6af7dcf1b267813af051f35341
student <you@eda.com>
2019-01-18 10:05:10 +1300

    added fs.readFile to routes

--- routes.js
+++ routes.js
@@ -3,8 +3,27 @@ const router = express.Router()
 const fs = require('fs')
 
 router.get('/', function (req, res) {
-    fs.readFile('./data.json','utf8', function(err,res))
-    res.render('home')
+    res.redirect('/poisons')
+})
+
+router.get('/poisons', function (req, res) {
+    fs.readFile('./data.json', function (err, data) {
+        if (err) return res.status(500).send("An error has occured :'(")
+
+        var poisonsObj = JSON.parse(data)
+        res.render('home', poisonsObj)
+    })
+})
+
+router.get('/poisons/:id', function (req, res) {
+    fs.readFile('./data.json', 'utf8', function (err, data) {
+        if (err) return res.status(500).send("An error has occured :'(")
+
+
+        var poisonsObj = JSON.parse(data)
+        console.log(poisonsObj)
+        res.render('poisons/poison', poisonsObj)
+    })
 })
 
 
