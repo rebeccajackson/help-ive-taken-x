512c5183e48c6926bd16ed54864501ddd7fa79e6
Katene Jack Cotton <katenejack@gmail.com>
2019-01-18 14:47:59 +1300

    fixed redirect from form to home

--- routes.js
+++ routes.js
@@ -45,15 +45,14 @@ router.get('/poisons/:id', function (req, res) {
 router.post('/edit', function (req, res) {
     fs.readFile('./data.json', 'utf8', function (err, data) {
         if (err) return res.status(500).send("An error has occured :'(")
-
+        var poisonsObj = JSON.parse(data)
         var newPoisonId = (poisonsObj.poisons.length + 1)
         // var id = Number(req.params.id)
-        // var poisonsObj = JSON.parse(data)
+        
         // var poison = poisonsObg.poison.find((poison) => poison.id === id)
         // res.render('poisons/edit', poison)
 
         var newClass = req.body.name.replace(/\s+/g, '-').toLowerCase()
-        console.log(newClass)
         // var newPoisonId =(poisonsObj.poisons.length +1);
         var newPoison = {
             id: newPoisonId,
